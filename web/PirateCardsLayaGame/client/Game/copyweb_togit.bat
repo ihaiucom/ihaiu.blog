3602d02671dd4a14ca6289b60736d397f2408e82
zengfeng75 <zengfeng75@gmail.com>
2020-04-27 00:03:17 +0800

    no message

---
+++ web/PirateCardsLayaGame/client/Game/copyweb_togit.bat
@@ -0,0 +1,4 @@
+(
+zffile copy release/web/version.json D:/zengfeng/githubs/gameweb/PirateCardsLayaGame/version.json
+zffile copy release/web/res/ D:/zengfeng/githubs/gameweb/PirateCardsLayaGame/res/
+)
